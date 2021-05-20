d9ae0542b9db6526fb0d4e73491d56a128afb20d
anapdh <ana_d_hubner@hotmail.com>
2021-05-20 18:08:58 -0300

    Add random landsape for background and styles for the card

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 import '../assets/styles/style.css';

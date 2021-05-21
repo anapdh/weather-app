5ab301989492fe36bf73e44727848ae51dad95ac
anapdh <ana_d_hubner@hotmail.com>
2021-05-21 19:27:01 -0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
@@ -6,33 +5,33 @@ import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 import '../assets/styles/style.css';
 
-let weather = {
-  "apiKey": "6122d350292c9bfc5150c7ce08ef1c41",
-  fetchWeather: function(city) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
+const weather = {
+  apiKey: '6122d350292c9bfc5150c7ce08ef1c41',
+  fetchWeather(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`)
       .then((response) => response.json())
       .then((data) => this.displayWeather(data));
   },
 
-  displayWeather: function(data) {
+  displayWeather(data) {
     const { name } = data;
-    const { icon, description } = data.weather[0]; //this property brings an array inside the object
+    const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    document.getElementById('city').innerText = "Weather in " + name
-    document.getElementById('temp').innerText = temp + "°C";
-    document.getElementById('icon').src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.getElementById('city').innerText = `Weather in ${name}`;
+    document.getElementById('temp').innerText = `${temp}°C`;
+    document.getElementById('icon').src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
     document.getElementById('description').innerText = description;
-    document.getElementById('humidity').innerText = "humidity: " + humidity + "%";
-    document.getElementById('wind').innerText = "wind: " + speed + "km/h";
-    
+    document.getElementById('humidity').innerText = `humidity: ${humidity}%`;
+    document.getElementById('wind').innerText = `wind: ${speed}km/h`;
+
     document.querySelector('.weather').classList.remove('loading');
-    document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+    document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${name}')`;
   },
 
-  search: function() {
+  search() {
     this.fetchWeather(document.getElementById('search-input').value);
-  }
+  },
 };
 
 document.getElementById('search-btn').addEventListener('click', () => {
@@ -40,9 +39,9 @@ document.getElementById('search-btn').addEventListener('click', () => {
 });
 
 document.getElementById('search-input').addEventListener('keyup', (e) => {
-  if (e.key == 'Enter') {
+  if (e.key === 'Enter') {
     weather.search();
   }
 });
 
-weather.fetchWeather("Paris");
+weather.fetchWeather('Paris');

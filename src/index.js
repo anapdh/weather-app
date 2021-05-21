0add9684c703f2ac69d15f5b7c7d4ca77ce611e3
anapdh <ana_d_hubner@hotmail.com>
2021-05-21 15:37:35 -0300

    Add api and api key

--- src/index.js
+++ src/index.js
@@ -5,3 +5,19 @@ import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 import '../assets/styles/style.css';
+
+let weather = {
+  "apiKey": "6122d350292c9bfc5150c7ce08ef1c41",
+  fetchWeather: function (city) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  },
+  displayWeather: function(data) {
+
+  }
+}
+
+//api.openweathermap.org/data/2.5/weather?q={city name}&units=metric&appid={API key}
+
+//api.openweathermap.org/data/2.5/weather?q=Porto Alegre&units=metric&appid=6122d350292c9bfc5150c7ce08ef1c41

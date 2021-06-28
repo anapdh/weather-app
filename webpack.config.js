c133956c05a6a5e2430890a95e82679d78ee0834
anapdh <ana_d_hubner@hotmail.com>
2021-06-28 16:43:03 -0300

    Update index.html to be in root

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
   },
   module: {
     rules: [
